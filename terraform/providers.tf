79c9fe80de21db4dada0a4ead62412bd1341e6bf
VinayakGlobant <vinayak.kadam@globant.com>
2025-12-09 11:39:50 +0530

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,11 +13,26 @@ terraform {
   }
 }
 
-provider "azurerm" {
-  features {}
-
-  # Prevent Terraform from trying provider registration (avoid unsupported arguments)
-  skip_provider_registration = true
+terraform {
+  required_version = ">= 1.0"
+  
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.75.0"
+    }
+  }
+  
+  backend "azurerm" {
+    # Backend configuration will be provided during init
+  }
 }
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  skip_provider_registration = true
   }
 }
